8652b0fbb78ddd97e260b12044609d019970f392
AceDemo1 <lawal641@gmail.com>
2024-07-31 21:33:23 -0700

    projects

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""This module defines a script that using an REST API, for a given employee ID
+and returns information about his/her"""
+
+
+if __name__ == "__main__":
+    import csv
+    import requests
+    from sys import argv
+
+    url = "https://jsonplaceholder.typicode.com"
+    user_id = argv[1]
+    user_url = "{}/users/{}".format(url, user_id)
+    todos_url = "{}/todos".format(user_url)
+
+    req_user = requests.get(user_url).json()
+    req_todos = requests.get(todos_url).json()
+
+    user_name = req_user.get('username')
+
+    filename = "{}.csv".format(user_id)
+    with open(filename, 'w', encoding='utf-8') as f:
+        csv_writer = csv.writer(f, delimiter=',', quoting=csv.QUOTE_ALL)
+
+        for value in req_todos:
+            data = [user_id, user_name, value.get('completed'),
+                    value.get('title')]
+            csv_writer.writerow(data)
+
+
---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""This module defines a script that using an REST API, for a given employee ID
+and returns information about his/her TODO list progress"""
+
+
+if __name__ == "__main__":
+    import json
+    import requests
+    from sys import argv
+
+    url = "https://jsonplaceholder.typicode.com"
+    user_id = argv[1]
+    user_url = "{}/users/{}".format(url, user_id)
+    todos_url = "{}/todos".format(user_url)
+
+    req_user = requests.get(user_url).json()
+    req_todos = requests.get(todos_url).json()
+
+    user_name = req_user.get('username')
+
+    filename = "{}.json".format(user_id)
+
+    list_element = []
+    for value in req_todos:
+        dictionary = {"task": value.get('title'),
+                      "completed": value.get('completed'),
+                      "username": user_name}
+        list_element.append(dictionary)
+    json_dictionary = {user_id: list_element}
+    to_json = json.dumps(json_dictionary)
+
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write(to_json)
+
+
