e160b91b072e1da406f451f61b4abba53c1acc8e
AceDemo1 <lawal641@gmail.com>
2024-08-03 20:20:47 -0700

    y

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,11 @@
+# Version compatible with Flask 2.1.0
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
 
 package { 'werkzeug':
-  ensure   => '2.0.3',  # Version compatible with Flask 2.1.0
+  ensure   => '2.0.3',  
   provider => 'pip3',
 }
 
