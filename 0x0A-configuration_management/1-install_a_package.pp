022e57244974595faa3f05ac4d90edb9bc79d66c
AceDemo1 <lawal641@gmail.com>
2024-08-03 20:19:16 -0700

    flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
-# Using Puppet, install flask from pip3
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
+
+package { 'werkzeug':
+  ensure   => '2.0.3',  # Version compatible with Flask 2.1.0
+  provider => 'pip3',
+}
+
