f3351f3cb626ee30949c794dde55be812057a0b7
AceDemo1 <lawal641@gmail.com>
2024-08-15 18:12:34 -0700

    puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,16 @@
+#automation
+package { 'nginx':
+	ensure => 'installed',
+}
+
+exec { 'header':
+	ensure => 'present',
+	path => '/etc/nginx/sites-available/default',
+	after => 'server_name_;',
+	line => 'add_header X-Served-By $hostname;',
+}
+
+service { 'nginx':
+	ensure => 'running',
+	require => Package['nginx'],
+}
