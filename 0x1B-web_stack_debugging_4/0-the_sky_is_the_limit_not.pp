85a482f0f43b7d849279e4aee5ad27401ef49375
AceDemo1 <lawal641@gmail.com>
2024-09-06 18:56:27 -0700

    fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,7 +3,7 @@
 exec { 'update_default_nginx':
   command => 'sed -i s/15/2000/ /etc/default/nginx',
   path    =>  [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ]
-} ->
+}
 
 exec { 'restart_nginx':
   command => 'sudo service nginx restart',
